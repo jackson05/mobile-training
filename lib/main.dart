2a5dc3d26d916fb8a2e07fb659ce95713ef8339f
jackson05 <jackson5g.bi@gmail.com>
2023-04-17 12:33:18 +0200

    trying to add login form

--- lib/main.dart
+++ lib/main.dart
@@ -70,14 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
 
       ),
-      /*
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
 
-       */
       bottomNavigationBar: BottomNavigationBar(
         items: [
            BottomNavigationBarItem(
---
+++ lib/user/login/login_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget{
+  const Login({ Key key }): super(key: key) {
+
+    @override
+    _LoginState createState() => _LoginState();
+  }
+  class LoginForm extends State<Login>{
+
+  }
+}
+
+
+
